1474cda987040070bb62e7de0daeec18d56b9f8c
NicolasDelFabro <nicolasdelfabro123@gmail.com>
2026-07-10 00:04:03 -0300

    correcion de importaciones

--- front/src/components/NavBar.tsx
+++ front/src/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import escudo from "../../public/general/logo.png";
+import escudo from "../../public/logo.png";
 
 const NavBar = () => {
   return (
